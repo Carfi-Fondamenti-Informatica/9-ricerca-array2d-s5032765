f0329e9d7e2ba340e845c33b1c2dbc2df0947ce0
s5032765 <91558903+s5032765@users.noreply.github.com>
2021-12-12 07:07:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <string>
 #include "lib.h"
+
 using namespace std;
 
 int main(){
 	char elenco[10][20];
-	char nome [20];
 	for(int i=0; i<10; i++)
 		cin>>elenco[i];
-}
+	string nome;
 	cin>>nome;
-	if(presente(nome,elenco)==-1){
+	if(presente(nome,elenco)==-1)
 		cout<<"non presente";
-	}else{
+	else
 		cout<<presente(nome,elenco);
-	}
 	return 0;
 }
